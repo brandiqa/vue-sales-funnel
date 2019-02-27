d54dd02ebef31ed36f0cd257d1cb8b7c77a343c6
Michael Wanyoike <micwanyoike@gmail.com>
2019-02-27 17:03:55 +0300

    Basic project setup

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Optin from "./views/Optin.vue";
+import ThankYou from "./views/Thank-you.vue";
 
 Vue.use(Router);
 
@@ -10,17 +11,13 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "optin",
+      component: Optin
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/thank-you",
+      name: "thank-you",
+      component: ThankYou
     }
   ]
 });
---
+++ src/views/Optin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="optin">
+    <b-row>
+      <b-col>
+        <p>Squeeze Funnel Page</p>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "optin"
+};
+</script>
