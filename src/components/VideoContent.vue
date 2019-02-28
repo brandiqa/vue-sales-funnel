ee0eb617bdfe7e0dff85790a327bbef3203fec54
Michael Wanyoike <micwanyoike@gmail.com>
2019-02-28 20:55:11 +0300

    Implemented video content and thank you page

---
+++ src/components/VideoContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="video-content" v-bind:style="{ marginTop: topMargin }">
+    <b-embed type="iframe" aspect="16by9" :src="link" allowfullscreen />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "video-content",
+  props: {
+    link: String,
+    topMargin: String
+  }
+};
+</script>
--- src/views/Thank-you.vue
+++ src/views/Thank-you.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="thank-you container">
+    <b-row>
+      <b-col>
+        <TextContent
+          variant="header"
+          content="Here’s Your Thank You Headline"
+          topMargin="50px"
+          theme="red"
+        />
+        <TextContent
+          variant="subheader"
+          content="This is your sub-headline to increase credibility or curiosity"
+        />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <VideoContent
+          link="https://www.youtube.com/embed/m9q58hSppds"
+          topMargin="30px"
+        />
+      </b-col>
+    </b-row>
   </div>
 </template>
+
+<script>
+import TextContent from "../components/TextContent.vue";
+import VideoContent from "../components/VideoContent";
+
+export default {
+  name: "thank-you",
+  components: {
+    TextContent,
+    VideoContent
+  }
+};
+</script>
